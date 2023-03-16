9db57a80d7050824cf30bb3487909d0df6edd989
ranaamr22 <ranaamr169@yahoo.com>
2023-03-16 15:32:08 -0700

    feat:Add new file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase with proper grammer
+ *
+ * Describtion: A c program that prints that the outcome is a piece of art
+ *
+ * Return: Always 0(success)
+ */
+
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
