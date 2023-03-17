1f8d7db7ab3f62a549e4288dc2ba1158ac0f9914
ranaamr22 <ranaamr169@yahoo.com>
2023-03-17 16:51:26 -0700

    loops and conditions

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/*
- * main - prints alphabets in lowercase then in upercase
- * Describtion: prints alphabets in lowercase then in upercase
+/**
+ * main - prints the alphabet in lowercase, and then in uppercase,
+ * followed by a new line
  * Return: Always 0 (Success)
  */
 
