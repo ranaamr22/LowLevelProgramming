dab559a9dff325b370e6f5fc69c74e41300342e4
ranaamr22 <ranaamr169@yahoo.com>
2023-04-10 12:51:02 -0700

    feat: add file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,7 +8,7 @@
  *
  * Return: Always 0 (success)
  */
-int main(int argc __attribute__((unsed)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 
