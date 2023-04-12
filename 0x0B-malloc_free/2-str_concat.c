fd118cecba6856f70bf57cf7dcc3f632b4d8cdda
ranaamr22 <ranaamr169@yahoo.com>
2023-04-11 19:30:47 -0700

    feat: add new file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * *str_concat - concatenates two strings
+ * str_concat - concatenates two strings
  * @s1: string to concatenate
  * @s2: other string to concatenate
  *
@@ -13,9 +13,9 @@ char *str_concat(char *s1, char *s2)
 	char *s3;
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	while (s1 && s1[len1])
+	while (s1 && *(s1 + len1) != '\0')
 		len1++;
-	while (s2 && s2[len2])
+	while (s2 && *(s2 + len2) != '\0')
 		len2++;
 
 	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
@@ -27,23 +27,24 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1)
 	{
-		while (i < len1)
+		while (*(s1 + i) != '\0')
 		{
-			s3[i] = s1[i];
+			*(s3 + i) = *(s1 + i);
 			i++;
 		}
 	}
 
 	if (s2)
 	{
-		while (i < (len1 + len2))
+		while (*(s2 + j) != '\0')
 		{
-			s3[i] = s2[j];
+			*(s3 + i) = *(s2 + j);
 			i++;
 			j++;
 		}
 	}
-	s3[i] = '\0';
+	*(s3 + i) = '\0';
 
 	return (s3);
 }
+
