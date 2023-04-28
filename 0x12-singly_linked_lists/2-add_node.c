e213bb33bb5def14c742cfc8e1df3e1427605cb5
ranaamr22 <ranaamr169@yahoo.com>
2023-04-27 17:54:25 -0700

    feat: add new file

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a linked list
+ * @head: double pointer to an element of type list_t
+ * @str: string to be added to the new node
+ * Return: the address of the new element
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *list;
+	int len = 0;
+
+	new = malloc(sizeof(list_t));
+	while (str[len])
+		len++
+	if (list != NULL)
+	{
+		list->str = strdup(str);
+		list->len = len;
+		list->next = (*head);
+		(*head) = list;
+	}
+	else
+		return (NULL);
+
+	return (*head);
+}
